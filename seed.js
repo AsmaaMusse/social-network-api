ed81522a9c310db0b9cfd3e89ed305a624f283c3
Asmaa <Asmaa99musse@gmail.com>
2022-02-11 03:13:21 +0000

    created a seed and sample file

---
+++ seed.js
@@ -0,0 +1,46 @@
+const users = [
+  {
+    username: "janeSmith",
+    email: "janeSmith@gmail.com",
+  },
+  {
+    username: "davidJones",
+    email: "davidJones@gmail.com",
+  },
+  {
+    username: "markelJames",
+    email: "markelJames@gmail.com",
+  },
+  {
+    username: "tomSmith",
+    email: "tmSmith@gmail.com",
+  },
+  {
+    username: "scotSteven",
+    email: "scotSteven@gmail.com",
+  },
+];
+
+const thoughts = [
+  {
+    thoughtText: "NoSQL is super easy",
+    username: "markelJames",
+    reactions: [
+      {
+        reactionId: "123",
+        reactionBody: "Yes I agree with you!!",
+        username: "davidJones",
+      },
+      {
+        reactionId: "456",
+        reactionBody: "Yup agree",
+        username: "tomSmith",
+      },
+      {
+        reactionId: "789",
+        reactionBody: "I am struggling with NoSQL",
+        username: "scotSteven",
+      },
+    ],
+  },
+];
