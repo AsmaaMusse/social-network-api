70642fdfc57605fe95d044561fb8c08e56725dcb
Asmaa <Asmaa99musse@gmail.com>
2022-02-13 21:47:33 +0000

    created controllers files

---
+++ src/controller/api/friendsController.js
@@ -0,0 +1,9 @@
+const { User } = require("../../models");
+
+const createFriend = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const deleteFriend = async (req, res) => {};
+
+module.exports = { createFriend, deleteFriend };
