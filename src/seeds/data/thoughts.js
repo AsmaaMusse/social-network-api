86b0db4273fc3ff83ec625a9ec2c8f5a2241c876
Asmaa <Asmaa99musse@gmail.com>
2022-02-13 20:34:53 +0000

    created seeding data

---
+++ src/seeds/data/thoughts.js
@@ -0,0 +1,60 @@
+const thoughts = [
+  {
+    thoughtText: "NoSQL is super easy",
+    username: "markelJames",
+    reactions: [
+      {
+        reactionBody: "Yes I agree with you!!",
+        username: "davidJones",
+      },
+      {
+        reactionBody: "Yup agree",
+        username: "tomSmith",
+      },
+      {
+        reactionBody: "I am struggling with NoSQL",
+        username: "scotSteven",
+      },
+    ],
+  },
+
+  {
+    thoughtText: "Learning new languages is easy",
+    username: "imaniJake",
+    reactions: [
+      {
+        reactionBody: "Couldn't agree more!!",
+        username: "connorCody",
+      },
+      {
+        reactionBody: "It's hard in my opinion",
+        username: "brianKurt",
+      },
+      {
+        reactionBody: "I am struggling with spanish",
+        username: "martinPhillips",
+      },
+    ],
+  },
+
+  {
+    thoughtText: "Can't wait for summer",
+    username: "ashleyAdam",
+    reactions: [
+      {
+        reactionBody: "Same here!",
+        username: "kellyAustin",
+      },
+      {
+        reactionBody: "Yup agree",
+        username: "kateJeffery",
+      },
+      {
+        reactionBody: "I prefer winter",
+        username: "sarahAndrew",
+      },
+    ],
+  },
+];
+
+module.exports = thoughts;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const db = require("./db/connection");
-const routes = require("./routers");
+const mongoose = require("mongoose");
 
-const PORT = process.env.port || 3003;
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 4000;
+
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+const init = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/studentStatsDb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("[INFO]: Database connection successful");
+
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(`[ERROR]: Database connection failed | ${error.message}`);
+  }
+};
+
+init();
