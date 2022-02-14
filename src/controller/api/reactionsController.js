dcf851f4cd9625901aa82c75267b3268f37e5c4e
Asmaa <Asmaa99musse@gmail.com>
2022-02-14 01:50:45 +0000

    completed the create and delete operations

--- src/controller/api/reactionsController.js
+++ src/controller/api/reactionsController.js
@@ -2,12 +2,44 @@ const { Thought } = require("../../models");
 
 const createReaction = async (req, res) => {
   try {
-  } catch (error) {}
+    const { thoughtId } = req.params;
+    const data = await Thought.findByIdAndUpdate(
+      thoughtId,
+      {
+        $push: { reactions: { ...req.body } },
+      },
+      { new: true }
+    );
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(
+      `[ERROR]: Failed to create a reaction for Thought | ${error.message}`
+    );
+    return res
+      .status(500)
+      .json({ success: false, Error: "Failed to create reaction" });
+  }
 };
 
 const deleteReaction = async (req, res) => {
   try {
-  } catch (error) {}
+    const { thoughtId, reactionId } = req.params;
+    const data = await Thought.findByIdAndUpdate(
+      thoughtId,
+      {
+        $push: { reactions: { _id: reactionId } },
+      },
+      { new: true }
+    );
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete reaction | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, Error: "Failed to delete reaction" });
+  }
 };
 
 module.exports = { createReaction, deleteReaction };
