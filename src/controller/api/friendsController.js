91f4bd7205a778b6af48d39a4106b6a042e6e06a
Asmaa <Asmaa99musse@gmail.com>
2022-02-14 01:40:42 +0000

    completed the create and delete operations

--- src/controller/api/friendsController.js
+++ src/controller/api/friendsController.js
@@ -2,12 +2,43 @@ const { User } = require("../../models");
 
 const createFriend = async (req, res) => {
   try {
-  } catch (error) {}
+    const { UserId } = req.params;
+    const data = await User.findByIdAndUpdate(
+      UserId,
+      {
+        $push: { friends: req.body._id },
+      },
+      { new: true }
+    ).populate("friends");
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create a friend for User`);
+    return res
+      .status(500)
+      .json({ success: false, Error: "Failed to create friend" });
+  }
 };
 
 const deleteFriend = async (req, res) => {
   try {
-  } catch (error) {}
+    const { userId, friendId } = req.params;
+    const data = await User.findByIdAndUpdate(
+      userId,
+      {
+        $pull: { friends: { $in: [friendId] } },
+      },
+      {
+        new: true,
+      }
+    );
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete friend`);
+    return res
+      .status(500)
+      .json({ success: false, Error: "Failed to delete friend" });
+  }
 };
 
 module.exports = { createFriend, deleteFriend };
